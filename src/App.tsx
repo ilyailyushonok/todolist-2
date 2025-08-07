6d322deb525236177520fb4995d57ad81d0f9b4f
ilya ilyushonok <ilyushonok2014@mail.ru>
2025-08-07 22:39:05 +0300

    refactor App 2

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ type todolistType = {
     title: string
     filter: string
 }
-
+//1
 type tasksStateType = { [todolistId: string]: tasksType[] }
 
 export type filteredtasksType = "all" | "active" | "completed"
